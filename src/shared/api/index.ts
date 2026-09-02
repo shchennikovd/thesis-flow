e10767fd754b7a69ba01be799582bfc06a87eb5b
Dima Shchennikov <shchennikovdp@yandex.ru>
2026-09-02 12:40:09 +0200

    fix(api): use relative urls instead of hardcoded localhost

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -2,7 +2,7 @@ import { ApiClient } from "./ApiClient";
 import { endpoints } from "./endpoints";
 
 // Пока заглушка URL (потом  вынести это в .env)
-export const apiClient = new ApiClient("http://localhost:3000/api");
+export const apiClient = new ApiClient("/api");
 
 // Экспортируем клиента и эндпоинты, чтобы сервисам было удобно их импортировать из одной точки
 export { endpoints };
